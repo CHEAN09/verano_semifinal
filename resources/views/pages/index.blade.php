7d52fc71678e2cd01068ccc26846cde4059f1e53
CHEAN09 <supermanverano@gmail.com>
2020-12-02 13:06:11 +0800

    First commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,32 @@
+@extends('base')
+
+
+@section('content')
+<div class="hero">
+    <div class="container" style="padding: 30px; color: #fff;">
+        <h2>Welcome All!</h2>
+    </div>
+</div>
+
+<div class="container" style="margin-top: 30px;">
+    <div class="row">
+        <div class="col">
+            <h3>Fundamentals</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="col">
+            <h3>Graphics Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="col">
+            <h3>Databases</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="col">
+            <h3>Advanced Dev</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </div>
+</div>
+
+@stop
